21ff9a2b9f13bc6390ac1228b3accdfd3a0bbed6
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2023-03-23 11:11:02 +0000

    updated Test game to extend Game

--- src/main/java/uk/co/dnbanks/gameengine/example/TestGame.java
+++ src/main/java/uk/co/dnbanks/gameengine/example/TestGame.java
@@ -3,11 +3,19 @@ package uk.co.dnbanks.gameengine.example;
 import uk.co.dnbanks.gameengine.game.Game;
 import uk.co.dnbanks.gameengine.utilities.Vector2D;
 
-public class TestGame {
+public class TestGame extends Game {
 
     public static void main(String[] args) {
-        Game.init();
-        Game.createScene("Main", new Vector2D(800, 800), true).setAsStartScene();
-        Game.launch();
+        // initialise the game - must be called first
+        initGame();
+        /* Creates a scene called main with the game window being 800x800
+          .setAsStartScene() is used here to set the start scene
+          alternatively you can call: Game.setStartScene("Main");
+          or to load the scene later on: Game.loadScene("Main");
+         */
+        createScene("Main", new Vector2D(800, 800), true).setAsStartScene();
+
+        // Lastly launching the game
+        launch();
     }
 }
