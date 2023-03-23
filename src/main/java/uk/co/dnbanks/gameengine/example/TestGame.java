0156bff395234a8e7f32d1359c06bdb76ca96b3f
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2023-03-23 23:37:29 +0000

    Added better comments to the example game

--- src/main/java/uk/co/dnbanks/gameengine/example/TestGame.java
+++ src/main/java/uk/co/dnbanks/gameengine/example/TestGame.java
@@ -6,11 +6,24 @@ import uk.co.dnbanks.gameengine.game.Scene;
 import uk.co.dnbanks.gameengine.objects.drawable.Rect;
 import uk.co.dnbanks.gameengine.utilities.Vector2D;
 
+/**
+ * Test class to show the features and the basics of the game engine.
+ * In my example to make it simple I've extended Game, you don't have to do this,
+ * but you'd have to call the static class Game before some functions bellow.
+ * NOTE: what I've said is subject to change I may change it, so you have to extend game and override a set start method
+ *
+ * @author Daniel Banks
+ */
 public class TestGame extends Game {
 
+    /**
+     * Main method to start the game.
+     * @param args arguments
+     */
     public static void main(String[] args) {
-        // initialise the game - must be called first
+        // initialise the game - MUST BE CALLED FIRST
         initGame();
+
         /* Creates a scene called main with the game window being 800x800
           .setAsStartScene() is used here to set the start scene
           alternatively you can call: Game.setStartScene("Main");
@@ -19,7 +32,7 @@ public class TestGame extends Game {
         Scene scene = createScene("Main", new Vector2D(800, 800), true);
         scene.setAsStartScene();
 
-        // Adding a rectangle to the scene with a size of 150x100 on position 100, 100 with the colour of aquq
+        // Adding a rectangle to the scene with a size of 150x100 on position 100, 100 with the colour of Aqua
         scene.addObject(new Rect("Test Rectangle", new Vector2D(150, 100), new Vector2D(100, 100), Color.AQUA));
 
 
